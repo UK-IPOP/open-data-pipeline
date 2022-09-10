977f8a3c4c51b33b6e70e9054b412fddbdcee8a7
Nick Anthony <nanthony007@gmail.com>
2022-09-09 20:28:19 -0400

    remove scripts

--- scripts/live_cli_test.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# use this script to run the CLI from test pypi
-
-pipx run \
-    --python $(which python) \
-    --index-url "https://test.pypi.org" \
-    opendata-pipeline --help
