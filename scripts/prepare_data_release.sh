23ee86be7586794e802e50d783c610245970050d
Nick Anthony <nanthony007@gmail.com>
2023-01-10 20:29:19 +0000

    Switch from tar to zip.

--- scripts/prepare_data_release.sh
+++ scripts/prepare_data_release.sh
@@ -2,20 +2,16 @@
 
 # This script prepares the data release.
 
-# general tar info
-# -c creates archive
-# -z uses gzip compression
-# -f specifies filename
-
 mkdir -p assets
 
-echo "Creating tarball zip of reports..."
-tar -czf assets/reports.tar.gz reports
+echo "Creating zip of reports..."
+# recurisve, verbose, highly compressed
+zip -rv9 assets/reports.zip reports/*.html 
 
-echo "Creating tarball zip of data sets..."
+echo "Creating zip of data sets..."
 for folder in data/*/; do
     echo "Processing $folder ..."
-    tar -czf assets/$(basename $folder).tar.gz $folder
+    zip -rv9 assets/$(basename $folder).zip $folder
 done
 
 echo "Copying spatial join form csv files to assets"
