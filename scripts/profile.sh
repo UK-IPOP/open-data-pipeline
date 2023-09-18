66cb8e3b340dae0225e46f30256b2256dd3f0b19
Nick Anthony <nanthony007@gmail.com>
2023-09-18 09:54:41 -0700

    Switch to ydata-profiling from pandas-profiling

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -10,7 +10,7 @@ for file in data/*_wide_form.csv; do
     # take action on each file. $f store current file name
     title=$(basename $file .csv)
 
-    pandas_profiling $file "reports/$title.html" \
+    ydata_profiling $file "reports/$title.html" \
         --title "Pandas Profiling Report for $title" \
         --infer_dtypes \
         --silent \
