6090f37bf222dfce2af0a4a8e142a9a00351e2ee
Nick Anthony <nanthony007@gmail.com>
2024-11-19 15:09:57 -0800

    Bump docker version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,17 +5,17 @@
 echo $GH_TOKEN | docker login ghcr.io -u nanthony007 --password-stdin
 
 echo "Building container..."
-docker build -f containers/cli.Dockerfile -t opendata-pipeline:0.2 .
+docker build -f containers/cli.Dockerfile -t opendata-pipeline:0.3.0 .
 
 # you also need to custom edit this tag
 
 echo "Tagging container..."
 # tag for ghcr.io
-docker tag bdc5d9c4e064 ghcr.io/uk-ipop/opendata-pipeline:0.2
+docker tag bdc5d9c4e064 ghcr.io/uk-ipop/opendata-pipeline:0.3.0
 
 echo "Publishing container..."
 # push to ghcr.io
-docker push ghcr.io/uk-ipop/opendata-pipeline:0.2
+docker push ghcr.io/uk-ipop/opendata-pipeline:0.3.0
 
 echo "Publishing PyPI package..."
 uv publish --build 
