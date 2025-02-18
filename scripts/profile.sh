f8e4b3319e92649c053e2316621692cd46473272
Nick Anthony <nanthony007@gmail.com>
2025-02-17 20:35:21 -0800

    fix bug? with binary name for profiling

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -10,7 +10,12 @@ for file in data/*_wide_form.csv; do
     # take action on each file. $f store current file name
     title=$(basename "$file" .csv)
 
-    uvx --python="3.12" --with="setuptools" ydata_profiling "$file" "reports/$title.html" \
+    uvx --python="3.12" \
+        --with="setuptools" \
+        --with="ydata-profiling" \
+        ydata_profiling \
+        "$file" \
+        "reports/$title.html" \
         --title "Pandas Profiling Report for $title" \
         --infer_dtypes \
         --silent \
